dcf17b1cd33b19502460d273ef5b0521675bd7ca
python <quang83693@gmail.com>
2022-07-30 23:21:53 +0700

    update file

---
+++ dedicated-server/README.md
@@ -0,0 +1,7 @@
+# CS:GO Dedicated server
+- The instruction is here https://developer.valvesoftware.com/wiki/Counter-Strike:_Global_Offensive_Dedicated_Servers
+- The startup_server.bat file originally come from **marcosolina**, check his file at https://github.com/marcosolina/csgo_server
+### Steam login token
+At this link https://steamcommunity.com/dev/managegameservers
+### Steam web API key
+At this link https://steamcommunity.com/dev/apikey
--- dedicated-server/startup_server.bat
+++ dedicated-server/startup_server.bat
@@ -1,11 +1,18 @@
-set STEAM_CSGO_KEY=C93D236D1F8B41506D41A120E66FA5AC
-set STEAM_API_KEY=00193A1ABA9D87129F78E5107134C2E0
-set CSGO_INSTALL_FOLDER_FOLDER=D:\csgo-ds\
-set MAP_GROUP=mg_active
-set MAP_START=de_mirage
-set HOST_IP=192.168.103.4
-set PORT_SV=27015
+set STEAM_CSGO_KEY=[YOUR_STEAM_KEY]
+set STEAM_API_KEY=[YOUR_STEAM_API_KEY]
+set CSGO_INSTALL_FOLDER_FOLDER=
+set PATH_STEAM_CMD=
+set MAP_GROUP=mg_classic_maps
+set MAP_START=mg_active
+set HOST_IP=[THE_IP_OF_YOUR_MACHINE]
 
 
-steamcmd +force_install_dir %CSGO_INSTALL_FOLDER_FOLDER% +login anonymous +app_update 740 +quit
-srcds -game csgo -console -usercon -port %PORT_SV% -ip %HOST_IP% +game_type 0 +game_mode 1 +mapgroup %MAP_GROUP% -tickrate 128 +map %MAP_START% +sv_setsteamaccount %STEAM_CSGO_KEY% -authkey %STEAM_API_KEY% -net_port_try 1
+%PATH_STEAM_CMD% +login anonymous +force_install_dir %CSGO_INSTALL_FOLDER_FOLDER% +app_update 740 +quit
+%CSGO_INSTALL_FOLDER_FOLDER%\srcds -game csgo -console -usercon -port 27015 +ip %HOST_IP% +game_type 0 +game_mode 1 +mapgroup %MAP_GROUP% +map %MAP_START% -authkey %STEAM_API_KEY% +sv_setsteamaccount %STEAM_CSGO_KEY% -net_port_try 1
+
+
+set /p DEL_DEMO_FILES=Delete *.dem files [y/n]?:
+
+IF /I "%DEL_DEMO_FILES%"=="y" (
+    del "%CSGO_INSTALL_FOLDER_FOLDER%\csgo\*.dem" /q
+    del "%CSGO_INSTALL_FOLDER_FOLDER%\csgo\backup_round*.txt" /q
